7398b3d5d628e7d7436055f2da47ff584d421b1f
ApuiIrom <114992036+ApuiIrom@users.noreply.github.com>
2023-06-13 00:15:39 +0530

    Update main.c

--- main.c
+++ main.c
@@ -1,46 +1,45 @@
-#include <limits.h>
-#include <stdbool.h>
 #include <stdio.h>
-#define V 4
-int minDistance(int dist[], bool sptSet[])
-{
-int min = INT_MAX, min_index;
-for (int v = 0; v < V; v++)
-if (sptSet[v] == false && dist[v] <= min)
-min = dist[v], min_index = v;
-return min_index;
-}
-void printSolution(int dist[])
-{
-printf("Vertex \t\t Distance from Source\n");
-for (int i = 0; i < V; i++)
-printf("%d \t\t\t\t %d\n", i+1, dist[i]);
-}
-void dijkstra(int graph[V][V], int src)
-{
-int dist[V];
-bool sptSet[V];
-for (int i = 0; i < V; i++)
-dist[i] = INT_MAX, sptSet[i] = false;
-dist[src] = 0;
-for (int count = 0; count < V - 1; count++) {
-int u = minDistance(dist, sptSet);
-sptSet[u] = true;
-for (int v = 0; v < V; v++)
-if (!sptSet[v] && graph[u][v] && dist[u]!=INT_MAX
-&& dist[u] + graph[u][v] < dist[v])
-dist[v] = dist[u] + graph[u][v];
-}
-printSolution(dist);
-}
+#include<limits.h>
 int main()
 {
-int i,j,graph[V][V];
-printf("Enter the adjacency matrix\n");
-for(i=0;i<V;i++)
-    for(j=0;j<V;j++){
-        scanf("%d",&graph[i][j]);
+    int n;
+    printf("Enter the no of nodes:");
+    scanf("%d",&n);
+    int count=0,i,j,graph[n][n],mincost=0,min,dist[n],visit[n],a,b;
+    printf("Enter the adjacency matrix\n");
+    for(i=0;i<n;i++){
+        dist[i]=INT_MAX;
+        visit[i]=0;
+        for(j=0;j<n;j++)
+            scanf("%d",&graph[i][j]);
     }
-dijkstra(graph,0);
-return 0;
+    visit[0]=1;
+    dist[0]=0;
+    printf("Vertex\t Distance from source\n");
+    while(count<n-1){
+        for(i=0;i<n && visit[i]==1;i++)/* visit[i]==1 means only from the visited node can continue the traversal source and
+            two or more nodes can be visited from the same visited node if the weight of the path is much more smaller
+            and this for loop is valid for visit[i]=1 so that's why we used visit[i]==1 inside the for loop condition
+            instead of using if condition after the for loop*/
+                for(j=0;j<n;j++)
+                   if(visit[j]==0 && graph[i][j] && dist[i]+graph[i][j]<=dist[j] && min>dist[i]+graph[i][j])/*visit[j]==0 is used so that
+                   the previous minimum weight of the visited node will not be used again
+                    i.e, if the current most min weight is from 1->2=3 so min=3 and it will be visited so visit[2]=1
+                     and the next most min weight is from say 2->4=5 or 1->3=5, which is less than 3 and their visit[4] and visit[3] is 0
+                     so the if condition will be fulfilled and hence the min=3 will not be used again
+                    **min value will update the most min weight after checking every possible weights available at the moment*/
+                        {
+                        dist[j]=dist[i]+graph[i][j];
+                        min=dist[j];
+                        a=i;
+                        b=j;
+                        }
+        mincost+=min;
+        count++;
+        printf("%d\t\t%d\n",b+1,min);
+        visit[b]=1;
+        min=INT_MAX;//we are giving min as INT_MAX so that min>dist[i]+graph[i][j] can be used
+    }
+    printf("The total min cost:%d",mincost);
+    return 0;
 }
