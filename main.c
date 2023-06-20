9bcd46a4a5c32a1bb98db68b14a5bbd19f7ad301
ApuiIrom <114992036+ApuiIrom@users.noreply.github.com>
2023-06-20 18:43:09 +0530

    Update main.c

--- main.c
+++ main.c
@@ -22,12 +22,14 @@ int main()
             and this for loop is valid for visit[i]=1 so that's why we used visit[i]==1 inside the for loop condition
             instead of using if condition after the for loop*/
                 for(j=0;j<n;j++)
-                   if(visit[j]==0 && graph[i][j] && dist[i]+graph[i][j]<=dist[j] && min>dist[i]+graph[i][j])/*visit[j]==0 is used so that
+                   if(visit[j]==0 && graph[i][j] && dist[i]+graph[i][j]<=dist[j] && min>dist[i]+graph[i][j])/*visit[j]==0 is used so that to avoid the loop or
                    the previous minimum weight of the visited node will not be used again
                     i.e, if the current most min weight is from 1->2=3 so min=3 and it will be visited so visit[2]=1
-                     and the next most min weight is from say 2->4=5 or 1->3=5, which is greater than 3 and their visit[4] and visit[3] is 0
-                     so the if condition will be fulfilled and hence the min=3 will not be used again
-                    **min value will update the most min weight after checking every possible weights available at the moment*/
+                     and the next most min weight is from say 2->4=5 or 1->3=5, which is greater than 3 and their visit[4] and visit[3] is 0 and visit[2] is 1
+                     so the if condition for visit[2] will not be fulfilled 
+                    **min is used to choose the shortest path to unvisited node which can be 
+                    travel at the moment from the source directly or indirectly through other visited nodes and min
+                    value will update the most min weight after checking every possible weights available at the moment*/
                         {
                         dist[j]=dist[i]+graph[i][j];
                         min=dist[j];
